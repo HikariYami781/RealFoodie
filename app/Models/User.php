bb8392e2204b2cb9eb6ca58a061331258d493c36
Paula Ledesma <134489317+HikariYami781@users.noreply.github.com>
2025-06-06 02:15:46 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,15 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
-/*use Laravel\Scout\Searchable;*/
 
 class User extends Authenticatable
 {
-  use HasFactory, Notifiable,  Authorizable/*, Searchable*/;
-
+    use HasFactory, Notifiable, Authorizable;
+    
     protected $fillable = [
         'nombre',         
         'email',
@@ -19,51 +17,56 @@ class User extends Authenticatable
         'foto_perfil',
         'descripcion'    
     ];
-
+    
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
+    
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
+    
     public function recetas()
     {
         return $this->hasMany(Receta::class);
     }
-
+    
     public function colecciones()
     {
         return $this->hasMany(Coleccion::class);
     }
-
+    
     public function comentarios()
     {
         return $this->hasMany(Comentario::class);
     }
-
+    
     public function valoraciones()
     {
         return $this->hasMany(Valoracion::class);
     }
-
+    
     public function recetasFavoritas()
     {
-        return $this->belongsToMany(Receta::class, 'favoritos')->withTimestamps();
+        return $this->belongsToMany(
+            Receta::class,     // Modelo relacionado
+            'favoritos',       // Tabla pivote
+            'user_id',         // Clave foránea del usuario en la tabla pivote
+            'receta_id'        // Clave foránea de la receta en la tabla pivote
+        )->withTimestamps();
     }
-
+    
     public function seguidores()
     {
         return $this->belongsToMany(User::class, 'seguidores', 'seguido_id', 'seguidor_id')->withTimestamps();
     }
-
+    
     public function siguiendo()
     {
         return $this->belongsToMany(User::class, 'seguidores', 'seguidor_id', 'seguido_id')->withTimestamps();
     }
-
+    
     public function toSearchableArray()
     {
         return [
@@ -74,5 +77,3 @@ class User extends Authenticatable
         ];
     }
 }
-
-/**Pasarle archivo al Claude y que me diga que está mal */
